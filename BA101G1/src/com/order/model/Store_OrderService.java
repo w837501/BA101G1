f96594223aae5f79eaa98d6b20fffee457e86807
Gjchiu <george33669@gmail.com>
2017-06-16 11:59:46 +0800

    push

---
+++ BA101G1/src/com/order/model/Store_OrderService.java
@@ -0,0 +1,76 @@
+package com.order.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import com.record.model.RecordVO;
+
+public class Store_OrderService {
+	
+	private Store_OrderDAO_interface dao;
+	
+	public Store_OrderService(){
+		dao = new Store_OrderDAO();
+	}
+	
+	public Store_OrderVO addOrder(String order_id, java.sql.Timestamp order_time, String mem_id, String store_id, Integer order_state, Integer totalprice, Integer order_way, String receive_address, byte[] qrcode, String order_note, Timestamp order_taketime ){
+		
+		Store_OrderVO orderVO = new Store_OrderVO();
+		
+		orderVO.setOrder_id(order_id);
+		orderVO.setOrder_time(order_time);
+		orderVO.setMem_id(mem_id);
+		orderVO.setStore_id(store_id);
+		orderVO.setOrder_state(order_state);
+		orderVO.setTotalprice(totalprice);
+		orderVO.setOrder_way(order_way);
+		orderVO.setReceive_address(receive_address);
+		orderVO.setQrcode(qrcode);
+		orderVO.setOrder_note(order_note);
+		orderVO.setOrder_taketime(order_taketime);
+		dao.insert(orderVO);
+		
+		return orderVO;
+		
+	}
+	
+	public Store_OrderVO updateRecord(String order_id, java.sql.Timestamp order_time, String mem_id, String store_id, Integer order_state, Integer totalprice, Integer order_way, String receive_address, byte[] qrcode, String order_note, Timestamp order_taketime){
+		
+		Store_OrderVO orderVO = new Store_OrderVO();
+		
+		orderVO.setOrder_id(order_id);
+		orderVO.setOrder_time(order_time);
+		orderVO.setMem_id(mem_id);
+		orderVO.setStore_id(store_id);
+		orderVO.setOrder_state(order_state);
+		orderVO.setTotalprice(totalprice);
+		orderVO.setOrder_way(order_way);
+		orderVO.setReceive_address(receive_address);
+		orderVO.setQrcode(qrcode);
+		orderVO.setOrder_note(order_note);
+		orderVO.setOrder_taketime(order_taketime);
+		dao.update(orderVO);
+		
+		return orderVO;
+	}
+	
+	public void deleteOrder(String order_id){
+		dao.delete(order_id);
+	}
+	
+	public Store_OrderVO getOneOrder(String order_id){
+		return dao.findByPrimaryKey(order_id);
+	}
+	
+	public List<Store_OrderVO>getAll(){
+		return dao.getAll();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
