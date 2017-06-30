791952f9815b68eb87469f5fe1a39cbd849eb591
15-ay118TX <15-ay118TX@Leo>
2017-06-30 16:37:05 +0800

    MODEL ORDER 訂單未確認狀態確認

--- BA101G1/src/com/order/model/Store_OrderDAO_interface.java
+++ BA101G1/src/com/order/model/Store_OrderDAO_interface.java
@@ -9,4 +9,5 @@ public interface Store_OrderDAO_interface {
 	public Store_OrderVO findByPrimaryKey(String order_id);
 	public List<Store_OrderVO> getAll();
 	public List<Store_OrderVO> findOrderByMem(String mem_id);
+	public List<Store_OrderVO> findOrderByState(String state);
 }
